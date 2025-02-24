72fde41277b825fc3a4e34b0002857d46bd2df97
Lucas Emiliano <88401857+lucas-elds@users.noreply.github.com>
2025-02-23 22:19:20 -0300

    Exemplo Botão de ação flutuante

--- app/src/main/java/com/example/pdm_floatingactionbutton/MainActivity.kt
+++ app/src/main/java/com/example/pdm_floatingactionbutton/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.ui.Alignment
 import com.example.pdm_floatingactionbutton.ui.theme.PDMFloatingActionButtonTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,10 +37,40 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
+        FloatingActionButton(
+            onClick = {},
+            containerColor = MaterialTheme.colorScheme.primary,
+            contentColor = Color.White
+        ) {
+            Icon(Icons.Filled.Add, contentDescription = "Floating Action Button")
+        }
+
+        SmallFloatingActionButton(
+            onClick = {},
+            containerColor = MaterialTheme.colorScheme.secondaryContainer,
+            contentColor = MaterialTheme.colorScheme.secondary
+        ) {
+            Icon(Icons.Filled.Add, contentDescription = "Small Floating Action Button")
+        }
+
+        LargeFloatingActionButton(
+            onClick = {},
+            shape = CircleShape,
+            containerColor = MaterialTheme.colorScheme.primary,
+            contentColor = Color.White
+        ) {
+            Icon(Icons.Filled.Add, contentDescription = "Large Floating Action Button")
+        }
+
+        ExtendedFloatingActionButton(
+            onClick = {},
+            icon = { Icon(Icons.Filled.Add, contentDescription = "Extended Floating Action Button Icon") },
+            text = { Text(text = "Extended FAB") },
+            containerColor = MaterialTheme.colorScheme.primary,
+            contentColor = Color.White
+        )
+    }
 }
 
 @Preview(showBackground = true)
